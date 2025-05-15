bed48d6a697641f45d77c9219f4d658e6d6833e0
Aziz94tg <aziz94tg@gmail.com>
2025-05-15 16:43:25 +0000

    A

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -30,9 +30,10 @@ def lazy_matrix_mul(m_a, m_b):
         msg = str(ve)
 
 
-        if "core dimension" in msg and "(1,0)" in msg:
-            raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
-        elif "core dimension" in msg and "(2,2)" in msg:
-            raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
-        else:
-            raise
+        if "core dimension" in msg:
+            if "(1,0)" in msg:
+                raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
+            elif "(2,2)" in msg and "(1,0)" in str(np.shape(m_b)):
+                raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
+
+        raise  
