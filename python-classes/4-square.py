d394ccee06292ebb027aaaf1a40a3b9facf49bb5
Aziz94tg <aziz94tg@gmail.com>
2025-05-20 12:33:48 +0000

    A

---
+++ python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with size and area method.
+It uses property decorators to control access to the private size attribute.
+"""
+
+
+class Square:
+    """Defines a square with private size attribute, with getter, setter and area method."""
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square. Defaults to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+           value:  The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate and returns the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
