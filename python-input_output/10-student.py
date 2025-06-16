0572e1fb7dcb726d7da815c6b7e4b9f426f1c3b1
Aziz94tg <aziz94tg@gmail.com>
2025-06-16 12:34:25 +0000

    A

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a class Student with optional filtered dictionary representation.
+"""
+
+
+class Student:
+    """Represents a student with public instance attributes."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance.
+
+        Args:
+            first_name: The student's first name.
+            last_name: The student's last name.
+            age: The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs: List of strings to filter attributes to include.
+
+        Returns:
+            dict: Dictionary of selected or all attributes.
+        """
+        if (type(attrs) == list and all(type(attr) == str for attr in attrs)):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        return self.__dict__
