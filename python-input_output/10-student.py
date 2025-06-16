b62d01edeff6806e401d1fea1e888137c7eb4b04
Aziz94tg <aziz94tg@gmail.com>
2025-06-16 12:39:39 +0000

    A

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -27,8 +27,7 @@ class Student:
         Returns:
             dict: Dictionary of selected or all attributes.
         """
-        if isinstance(attrs, list) and all(isinstance(attr, str)\
-                for attr in attrs):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
             return {
                     key: self.__dict__[key]
                     for key in attrs if key in self.__dict__
