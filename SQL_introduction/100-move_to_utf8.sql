fe5d41f06cfff9bae60487bb1898950aae7d9c5b
Aziz94tg <aziz94tg@gmail.com>
2025-06-23 15:27:04 +0000

    A

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Convert the database to utf8mb4 with utf8mb4_unicode_ci collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table to utf8mb4 with utf8mb4_unicode_ci collation
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Ensure the 'name' column uses utf8mb4_unicode_ci
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
