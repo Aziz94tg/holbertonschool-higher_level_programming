e955d27d5e3d6cea2c5e226602b748f6417e70f3
Aziz94tg <aziz94tg@gmail.com>
2025-06-23 15:28:14 +0000

    A

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,7 @@
 -- Convert the database to utf8mb4 with utf8mb4_unicode_ci collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
+USE hbtn_0c_0;
 -- Convert the table to utf8mb4 with utf8mb4_unicode_ci collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
