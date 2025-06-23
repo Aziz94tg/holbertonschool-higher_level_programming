54d0483ede6bf266ad8b5c72c9ef4bf504e9ec60
Aziz94tg <aziz94tg@gmail.com>
2025-06-23 15:31:50 +0000

    A

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
--- Step 1: Set the database charset and collation
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Step 1: Convert database to UTF8 (utf8mb4 + utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE hbtn_0c_0;
 
