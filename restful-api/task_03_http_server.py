dbd81e9e9276ca9afa49686636f29d258c86f167
Aziz94tg <aziz94tg@gmail.com>
2025-06-23 14:40:13 +0000

    A

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -18,25 +18,18 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b'{"message":"OK"}')  # <-- No spaces
-
-        elif self.path == "/info":
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(b'{"version":"1.0","description":"A simple API built with http.server"}')
+            self.wfile.write(b'{"message":"OK"}')
 
         else:
             self.send_response(404)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b'{"message":"Not Found"}')  # <-- No space
+            self.wfile.write(b'{"message":"Not Found"}')
 
-def run(server_class=HTTPServer, handler_class=SimpleAPIHandler):
-    port = 8000
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Starting HTTP server on http://localhost:{port}")
+def run():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    print("Starting server at http://localhost:8000")
     httpd.serve_forever()
 
 if __name__ == "__main__":
