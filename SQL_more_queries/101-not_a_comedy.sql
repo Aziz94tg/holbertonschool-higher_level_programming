9ff10f76d047ce9ea2e76d88ae13ae5390980d90
Aziz94tg <aziz94tg@gmail.com>
2025-06-24 13:25:50 +0000

    A

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,11 @@
--- List all TV shows without the genre Comedy
-SELECT title
-FROM tv_shows
-WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (
-        SELECT id FROM tv_genres WHERE name = 'Comedy'
-    )
-)
-ORDER BY title ASC;
+-- Script that lists all shows without the genre Comedy
 
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
