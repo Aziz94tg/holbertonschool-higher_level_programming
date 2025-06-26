69a85b42a61c73232d681e3ed7ea5b738e8f26e6
Aziz94tg <aziz94tg@gmail.com>
2025-06-26 13:28:09 +0000

    A

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,37 +1,59 @@
 #!/usr/bin/python3
 """
-Lists all cities of a given state from the database hbtn_0e_4_usa.
-Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
+Script that takes in the name of a state as an argument and lists all cities
+of that state, using the database hbtn_0e_4_usa
 """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    # Connect to the database
+def filter_cities():
+    """
+    Lists all cities of a given state from the database
+    Takes 4 arguments: mysql username, mysql password, database name and state name
+    Results are sorted in ascending order by cities.id
+    Safe from MySQL injection by using parameterized queries
+    Uses only one execute() call
+    """
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    
+    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=db_name
+        db=database,
+        charset="utf8"
     )
+    
+    # Create cursor object
     cursor = db.cursor()
-
-    query = (
-        "SELECT cities.name FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id ASC"
-    )
-
+    
+    # Execute SQL query using JOIN to get cities for the specified state
+    # Using parameterized query to prevent SQL injection
+    query = """SELECT cities.name FROM cities 
+               INNER JOIN states ON cities.state_id = states.id 
+               WHERE states.name = %s 
+               ORDER BY cities.id ASC"""
     cursor.execute(query, (state_name,))
+    
+    # Fetch all results
     results = cursor.fetchall()
-
-    print(", ".join([city[0] for city in results]))
-
+    
+    # Extract city names and join them with commas
+    city_names = [row[0] for row in results]
+    print(", ".join(city_names))
+    
+    # Close cursor and database connection
     cursor.close()
     db.close()
 
+
+if __name__ == "__main__":
+    filter_cities()
