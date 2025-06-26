b4b7363fef92e6c0feec54e4123f370714b85844
Aziz94tg <aziz94tg@gmail.com>
2025-06-26 13:11:33 +0000

    A

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
+Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
 """
 
 import MySQLdb
 import sys
 
-
-def filter_states():
-    """
-    Lists all states with a name starting with N from the database
-    Takes 3 arguments: mysql username, mysql password and database name
-    Results are sorted in ascending order by states.id
-    """
-    # Get command line arguments
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
-    # Connect to MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-    
-    # Create cursor object
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
-    
-    # Execute SQL query to select states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-    
-    # Fetch all results
-    results = cursor.fetchall()
-    
-    # Print results
-    for row in results:
+
+    # Execute the query
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    # Fetch and display results
+    for row in cursor.fetchall():
         print(row)
-    
-    # Close cursor and database connection
+
+    # Clean up
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    filter_states()
