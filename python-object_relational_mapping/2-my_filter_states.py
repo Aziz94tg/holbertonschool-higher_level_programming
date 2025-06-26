ad8ed78d08b3be2603d35c8afedeb3e6138e35d3
Aziz94tg <aziz94tg@gmail.com>
2025-06-26 14:27:02 +0000

    A

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ import sys
 def filter_states_by_input():
     """
     Displays all values in the states table where name matches the argument
-    Takes 4 arguments: mysql username, mysql password, database name and state name
+    arguments: mysql username, mysql password, database name and state name
     Results are sorted in ascending order by states.id
     Uses format to create the SQL query with user input
     """
@@ -20,7 +20,6 @@ def filter_states_by_input():
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
-    
     # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
@@ -30,25 +29,23 @@ def filter_states_by_input():
         db=database,
         charset="utf8"
     )
-    
     # Create cursor object
     cursor = db.cursor()
-    
     # Execute SQL query using format to insert user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER\
+            BY id ASC".format(state_name)
     cursor.execute(query)
-    
     # Fetch all results
     results = cursor.fetchall()
-    
     # Print results
     for row in results:
         print(row)
-    
+
     # Close cursor and database connection
     cursor.close()
     db.close()
 
 
 if __name__ == "__main__":
+
     filter_states_by_input()
