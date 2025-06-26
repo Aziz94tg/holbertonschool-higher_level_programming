148dfec379efbe55c2048ef2015b6efed2681786
Aziz94tg <aziz94tg@gmail.com>
2025-06-26 13:14:58 +0000

    A

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,48 @@
 #!/usr/bin/python3
 """
-Safely filters states by name using parameterized query to avoid SQL injection.
-Usage: ./3-my_safe_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>
+Script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+This version is safe from MySQL injections!
 """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+
+def safe_filter_states():
+    """
+    Displays all values in the states table where name matches the argument
+    Takes 4 arguments: mysql username, mysql password, database name and state name
+    Results are sorted in ascending order by states.id
+    Safe from MySQL injection by using parameterized queries
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+    
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
-
-    for row in cursor.fetchall():
+    
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    
+    results = cursor.fetchall()
+    
+    for row in results:
         print(row)
-
+    
     cursor.close()
     db.close()
 
+
+if __name__ == "__main__":
+    safe_filter_states()
