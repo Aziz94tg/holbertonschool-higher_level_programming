851cf4a480cfb31504702ed8e1838f2f6277077d
Aziz94tg <aziz94tg@gmail.com>
2025-07-10 15:51:04 +0000

    Add

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+
+myObject.incr = function () {
+  this.value++;
+};
   
-  // Add a new method to increment the value
-  myObject.incr = function () {
-    this.value++;
-  };
-  
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
   
